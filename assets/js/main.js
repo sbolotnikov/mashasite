4ca556d5a4c39a9d75673fab7876e84c0eaed0ab
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-12-13 12:48:08 -0500

    animated sale gallary

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,72 @@ var totalPictures = 39;
 var totalArtworks=16;
 var emailToSend="sergey@time-dance.com";
 // ----------------------------------------------
+
+const images = [
+    "../assets/images/artworks/gallery1.webp",
+    "../assets/images/artworks/gallery2.webp",
+    "../assets/images/artworks/gallery3.webp",
+];
+
+// content setup
+const texts = [
+    ["Saturn", "ink, pencils, acryl and crystals on paper"],
+    ["Flower Fairies", "Ink, colored pencils, acryl, crystals on paper."],
+    ["Lady of Marshland", "for sale! 270 euro, free delivery. watercolor on paper"],
+]
+
+rgbKineticSlider = new rgbKineticSlider({
+
+    slideImages: images, // array of images > must be 1920 x 1080
+    itemsTitles: texts, // array of titles / subtitles
+
+    backgroundDisplacementSprite: '../assets/images/stripe1.png', // slide displacement image 
+    cursorDisplacementSprite: '../assets/images/displace-circle.png', // cursor displacement image
+
+    cursorImgEffect : true, // enable cursor effect
+    cursorTextEffect : false, // enable cursor text effect
+    cursorScaleIntensity : 0.65, // cursor effect intensity
+    cursorMomentum : 0.14, // lower is slower
+
+    swipe: true, // enable swipe
+    swipeDistance : window.innerWidth * 0.4, // swipe distance - ex : 580
+    swipeScaleIntensity: 2, // scale intensity during swipping
+
+    slideTransitionDuration : 1, // transition duration
+    transitionScaleIntensity : 30, // scale intensity during transition
+    transitionScaleAmplitude : 160, // scale amplitude during transition
+
+    nav: true, // enable navigation
+    navElement: '.main-nav', // set nav class
+    
+    imagesRgbEffect : false, // enable img rgb effect
+    imagesRgbIntensity : 0.9, // set img rgb intensity
+    navImagesRgbIntensity : 80, // set img rgb intensity for regular nav 
+
+    textsDisplay : true, // show title
+    textsSubTitleDisplay : true, // show subtitles
+    textsTiltEffect : true, // enable text tilt
+    googleFonts : ['brandongrotesque:900', 'brandongrotesque:800'], // select google font to use
+    buttonMode : false, // enable button mode for title
+    textsRgbEffect : true, // enable text rgb effect
+    textsRgbIntensity : 0.03, // set text rgb intensity
+    navTextsRgbIntensity : 15, // set text rgb intensity for regular nav
+
+    textTitleColor : 'white', // title color
+    textTitleSize : 85, // title size
+    mobileTextTitleSize : 50, // title size
+    textTitleLetterspacing : 3, // title letterspacing
+
+    textSubTitleColor : 'white', // subtitle color ex : 0x000000
+    textSubTitleSize : 15, // subtitle size
+    mobileTextSubTitleSize : 12, // mobile subtitle size
+    textSubTitleLetterspacing : 2, // subtitle letter spacing
+    textSubTitleOffsetTop : 200, // subtitle offset top
+    mobileTextSubTitleOffsetTop : 200, // mobile subtitle offset top
+});
+
+
+
 var animationArray=[['slower',''],['faster', ''],['slower', 'vertical'],['slower', 'slower-down'],['',''],['slower',''],['faster1',''],['slower', 'slower1'],['',''],['slower', 'slower1'],['faster1','']]
 var windWidth = window.innerWidth;
 var windHeight = window.innerHeight;
